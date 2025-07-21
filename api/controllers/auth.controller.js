50ec5f21b795b3f8f669043697743dbfa2dfe375
gvpmoneesha <moneeshakavindi@gmail.com>
2025-07-22 00:50:19 +0530

    update user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -96,7 +96,14 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
-      res.status(200);
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
     }
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
