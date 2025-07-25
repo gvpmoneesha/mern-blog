75cf02b3d74060f2a77bb3e9ca6990e95626ada7
gvpmoneesha <moneeshakavindi@gmail.com>
2025-07-25 23:58:30 +0530

    create get users api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -90,7 +90,6 @@ export const deletepost = async (req, res, next) => {
 };
 
 export const updatepost = async (req, res, next) => {
-  console.log(req);
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to update the post"));
   }
